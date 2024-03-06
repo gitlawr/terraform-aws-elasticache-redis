b21a2cbd0b59652efd3d14e9e8b083619311d398
Lawrence Li <lawrleegle@gmail.com>
2024-03-06 15:00:56 +0800

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -3,25 +3,8 @@
 #
 
 variable "context" {
-  description = <<-EOF
-Receive contextual information. When Walrus deploys, Walrus will inject specific contextual information into this field.
-
-Examples:
-```
-context:
-  project:
-    name: string
-    id: string
-  environment:
-    name: string
-    id: string
-  resource:
-    name: string
-    id: string
-```
-EOF
-  type        = map(any)
-  default     = {}
+  description = "context"
+  type        = any
 }
 
 #
